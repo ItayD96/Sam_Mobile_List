ca1f6c18f37432f7e9b542c347627604650d02b9
ItayD96 <itaydahan96@gmail.com>
2021-10-06 00:54:25 +0300

    Added auto scan - No need input model list

--- appAndParse.py
+++ appAndParse.py
@@ -1,94 +1,102 @@
 ﻿#!/isr/bin/python3
 
 import json
+from optparse import OptionParser
+
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import xlrd
-from openpyxl import Workbook, load_workbook
-from optparse import OptionParser
 
 
 def main():
     # Data to be written
     parser = OptionParser()
-    parser.add_option('-i', '--input', type='string', help='The excel file to convert')
+    parser.add_option('-i', '--input_list', type='string',
+                      help='The Available samsung models https://www.sammobile.com/samsung/')
     parser.add_option('-o', '--outfile', help='Final Json file name')
 
     (options, args) = parser.parse_args()
 
-    if (options.input is None):
-        worksheet = pd.read_excel('getFrim/Models and Firmwares.xls')
+    if options.input_list is None:
+        print('Must enter list of models visit here : https://www.sammobile.com/samsung/')
+        model_list = open('inputModelList.txt', 'r')  # Just for me
+        # exit()
     else:
-        if (options.input[-4:-1] == '.xls'):
-            worksheet = pd.read_excel('getFrim/Models and Firmwares.xls')
+        if options.outfile[-4:] == '.txt':
+            model_list = open(options.input_list, 'r')
         else:
-            worksheet = pd.read_excel(options.input + '.xls')
-        if (worksheet is None):
-            print('Fail to read the input txt file')
-            exit()
+            model_list = open(options.input_list + '.txt', 'r')
 
-    if (options.outfile is None):
+    if options.outfile is None:
         output = open("output.json", "w")
     else:
-        if (options.outfile[-4:] == '.json'):
+        if options.outfile[-5:] == '.json':
             output = open(options.outfile, 'w')
         else:
-            output = open(options.outfile +'.json', 'w')
+            output = open(options.outfile + '.json', 'w')
 
     # Json headers
     headers = ["Model", "Firmware", "Country", "Date", "PDA", "Version"]
     # Header counter
-    i =0
+    i = 0
     # Tmp dict
     dict1 = {}
-    # Get the file path+name.
-    worksheet = pd.read_excel('getFrim/Models and Firmwares.xls', sheet_name='Models and Firmwares')
 
-
-    for model in worksheet.columns:
-        j = 0                           # j for the frimware number
-        firmware = worksheet.at[j, model]
-
-        last = worksheet[model].iloc[[-1]].astype(str).tolist()
-        last = ''.join(last)
-
+    for line in model_list.readlines():
+        # use req.get with dynamic url from the list
+        model = line.replace(' ', '-').replace('+', '-plus').lower()[:-1]
+        result_main = requests.get('https://www.sammobile.com/samsung/' + model + '/firmware/')
         print(model)
-        while (str(firmware) != last ):  # while there are more firmwares
+        # check valid address
+        if result_main.status_code != 200:
+            print('Error , status code != 200,Model :' + model)
+            continue
 
-            firmware = worksheet.at[j, model]
-            result = requests.get("https://www.sammobile.com/samsung/" + str(model) + "/firmware/#" + str(firmware))
+        else:
 
-            if result.status_code != 200:
-                print("Error , status code != 200,Exit.")
-                exit()
+            # get all the buttons that mean avail options for firmware
+            src_main = result_main.content
+            soup_main = BeautifulSoup(src_main, 'lxml')
+            firmware_list = soup_main.find('div', attrs={'class': "main-content-item__subtitle nav"})
+            firmware_list = firmware_list.find_all('button', {'class': 'nav-link badge badge-success-outline'})
+            for firmware_button in firmware_list:
+                firmware = firmware_button.get_text().strip('\n').strip(' ')
 
-            src = result.content
-            soup = BeautifulSoup(src, 'lxml')
-            table = soup.find('div', attrs={'id': firmware})
-            if (table is None):
-                # print('Table is None !!')
-                break
-            else:
-                for row in table.findAll('td'):
-                    tmp = row.get_text()
-                    if tmp != 'Model' and tmp != 'Country/Carrier' and tmp != 'Date' and tmp != 'PDA' and tmp != 'Version':
-                        if (i == 0):  # Update the model every time
-                            dict1[str(headers[i])] = model.strip('\n')
+                result = requests.get("https://www.sammobile.com/samsung/" + str(model) + "/firmware/#" + str(firmware))
+
+                if result.status_code != 200:
+                    print('Error , status code != 200,Model : ' + str(model + ' Firmware : ' + firmware))
+
+                    exit()
+
+                src = result.content
+                soup = BeautifulSoup(src, 'lxml')
+                table = soup.find('div', attrs={'id': firmware})
+                if table is None:
+                    # print('Table is None !!')
+                    break
+                else:
+                    for row in table.findAll('td'):
+                        tmp = row.get_text()
+                        if [tmp != 'Model' and
+                            tmp != 'Country/Carrier' and
+                            tmp != 'Date' and
+                            tmp != 'PDA' and
+                            tmp != 'Version']:
+                            if i == 0:  # Update the model every time
+                                dict1[str(headers[i])] = model.strip('\n')
+                                i += 1
+
+                            dict1[str(headers[i])] = tmp.strip('\n')  # strip('\n') to cut of /n , inset to the json
+                            if i == 5:
+                                json.dump(dict1, output, indent=6, sort_keys=False)
+                                i = -1  # Reset the counter
+
                             i += 1
-
-                        dict1[str(headers[i])] = tmp.strip('\n')  # strip('\n') to cut of /n , inset to the json
-                        if (i == 5):
-                            json.dump(dict1, output, indent=6, sort_keys=False)
-                            i = -1  # Reset the counter
-
-                        i += 1
-
-            j += 1
 
     print('Finish , File named ' + output.name + ' has created')
 
     output.close()
+    model_list.close()
 
 
 if __name__ == "__main__":
---
+++ sam_model_prefinal.py
@@ -0,0 +1,97 @@
+﻿#!/isr/bin/python3
+
+import json
+from optparse import OptionParser
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    # Data to be written
+    parser = OptionParser()
+    parser.add_option('-o', '--outfile', help='Final Json file name')
+
+    (options, args) = parser.parse_args()
+
+    if options.outfile is None:
+        output = open("output.json", "w")
+    else:
+        if options.outfile[-5:] == '.json':
+            output = open(options.outfile, 'w')
+        else:
+            output = open(options.outfile + '.json', 'w')
+
+    # Json headers
+    headers = ["Model", "Firmware", "Country", "Date", "PDA", "Version"]
+    # Header counter
+    i = 0
+    # Tmp dict
+    dict1 = {}
+
+    model_list = requests.get(' https://www.sammobile.com/samsung/')
+    src_model_list = model_list.content
+    soup_model_list = BeautifulSoup(src_model_list, 'lxml')
+    model_list = soup_model_list.find_all("strong")
+    for model in model_list:
+        #print(model.get_text())
+        if str(model.get_text())[:6].lower() == 'galaxy':
+            model = model.get_text().replace(' ', '-').replace('+', '-plus').lower()  # Changed !
+
+            result_main = requests.get('https://www.sammobile.com/samsung/' + model + '/firmware/')
+
+            # check valid address
+            if result_main.status_code != 200:
+                print('Error , status code != 200,Model :' + model)
+                continue
+
+            else:
+
+                # get all the buttons that mean avail options for firmware
+                src_main = result_main.content
+                soup_main = BeautifulSoup(src_main, 'lxml')
+                firmware_list = soup_main.find('div', attrs={'class': "main-content-item__subtitle nav"})
+                firmware_list = firmware_list.find_all('button', {'class': 'nav-link badge badge-success-outline'})
+
+                for firmware_button in firmware_list:
+                    firmware = firmware_button.get_text().strip('\n').strip(' ')
+
+                    result = requests.get(
+                        "https://www.sammobile.com/samsung/" + str(model) + "/firmware/#" + str(firmware))
+
+                    if result.status_code != 200:
+                        print('Error , status code != 200,Model : ' + str(model + ' Firmware : ' + firmware))
+                        #
+                        # exit()
+                    # get all data from the main table
+                    src = result.content
+                    soup = BeautifulSoup(src, 'lxml')
+                    table = soup.find('div', attrs={'id': firmware})  # find the main table
+                    if table is None:
+                        # print('Table is None !!')
+                        break
+                    else:
+                        for row in table.findAll('td'):  # find all the rows
+                            tmp = row.get_text()
+                            if tmp != 'Model' and tmp != 'Country/Carrier' and \
+                                    tmp != 'Date' and tmp != 'PDA' and tmp != 'Version':
+                                if i == 0:  # Update the model every time
+                                    dict1[str(headers[i])] = model.strip('\n')
+                                    i += 1
+
+                                dict1[str(headers[i])] = tmp.strip('\n')  # strip('\n') to cut of /n , inset to the json
+                                if i == 5:
+                                    json.dump(dict1, output, indent=6, sort_keys=False)
+                                    i = -1  # Reset the counter
+
+                                i += 1
+
+    print('Finish , File named ' + output.name + ' has created')
+
+    output.close()
+    #model_list.close()
+
+
+if __name__ == "__main__":
+    main()
+    exit()
